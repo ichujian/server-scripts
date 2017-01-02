32274412ab8bbc1b94583395ba1c2cc20a78244c
fyibmsd <fyibmsd@gmail.com>
2017-01-02 18:52:20 +0800

    update

--- dotfile.sh
+++ dotfile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -sL https://coding.net/u/fyibmsd/p/zsh/git/raw/master/init.sh | bash -
-curl -sL https://coding.net/u/fyibmsd/p/vim/git/raw/master/init.sh | bash -
+curl -sL https://raw.githubusercontent.com/fyibmsd/dotzsh/master/init.sh | bash -
+curl -sL https://raw.githubusercontent.com/fyibmsd/dotvim/master/init.sh | bash -
 
 
