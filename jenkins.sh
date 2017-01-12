abeb971058d5bb86e9fef1f705b9ede81b5997ae
root <fyibmsd>
2017-01-12 11:18:18 +0800

    add jenkins

---
+++ jenkins.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum install jenkins -y
+
